6dab1d5f67bc52840728866e1ee8b0d4b914d4a2
sorz2122 <81297488+sorz2122@users.noreply.github.com>
2026-08-12 21:14:20 +0200

    Create manifest.js

---
+++ public/manifest.js
@@ -0,0 +1,11 @@
+{
+  "name": "Immich Album Sync",
+  "short_name": "Album Sync",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#08090c",
+  "theme_color": "#08090c",
+  "icons": [
+    { "src": "/immich-logo.svg", "sizes": "any", "type": "image/svg+xml", "purpose": "any" }
+  ]
+}
